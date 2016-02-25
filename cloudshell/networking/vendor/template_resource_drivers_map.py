48327719a07defda4d0a6474b95bec8c702b3ec7
CoYe <wiseman.kostya@gmail.com>
2016-02-25 16:59:09 +0200

    cleanup master from old CS version

--- cloudshell/networking/vendor/template_resource_drivers_map.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'CoYe'
-
-CISCO_RESOURCE_DRIVERS_MAP = \
-    {
-        '359': 'BROCADE'
-    }
-# Please refer to SNMPV2-MIB sysObjectID command to retrieve last numbers of your device
-# example output = 'SNMPv2-SMI::enterprises.9.1.359'
